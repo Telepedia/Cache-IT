099f6a4137540856bd3b1ccb2df5e265f40445ff
OriginalAuthority <luke@telepedia.net>
2025-11-09 22:03:51 +0000

    Add some redirects

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,50 @@
 import { MediaWikiRequest } from "./MediaWikiRequest";
 import { log } from "./logger";
 
+const DOMAIN_REDIRECTS: Record<string, { newDomain: string; pathPrefix?: string }> = {
+    'latelierdessorciers.telepedia.net': {
+        newDomain: 'witchhatatelier.telepedia.net',
+        pathPrefix: '/fr'
+    },
+	'elatelierdesombrerosdemago.telepedia.net': {
+		newDomain: 'witchhatatelier.telepedia.net',
+		pathPrefix: '/es'
+	},
+	'atelierspiczastychkapeluszy.telepedia.net': {
+		newDomain: 'witchhatatelier.telepedia.net',
+		pathPrefix: '/pl'
+	}
+};
+
+// Check if we need to redirect this domain from its old url to the new
+// language path variant
+function checkDomainRedirect(request: Request): Response | null {
+    const url = new URL(request.url);
+    const redirect = DOMAIN_REDIRECTS[url.hostname];
+    
+    if (redirect) {
+        const newUrl = new URL(url.href);
+        newUrl.hostname = redirect.newDomain;
+        
+        if (redirect.pathPrefix) {
+            newUrl.pathname = redirect.pathPrefix + url.pathname;
+        }
+        
+        return Response.redirect(newUrl.toString(), 301);
+    }
+    
+    return null;
+}
+
 export default {
 	async fetch( req, env, ctx ): Promise<Response> {
 		ctx.passThroughOnException();
+
+		const redirect = checkDomainRedirect(req);
+		
+        if (redirect) {
+            return redirect;
+        }
 
 		const request = new MediaWikiRequest( req, env );
 
